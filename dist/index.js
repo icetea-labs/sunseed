76f7f6c837ad8879f93c5e8e1918b1dbe18411e7
Truong Hong Thi <thi1981@gmail.com>
2019-11-21 11:05:07 +0700

    Update dependencies

--- dist/index.js
+++ dist/index.js
@@ -2,11 +2,13 @@
 
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
-var _objectSpread2 = _interopRequireDefault(require("@babel/runtime/helpers/objectSpread"));
+var _defineProperty2 = _interopRequireDefault(require("@babel/runtime/helpers/defineProperty"));
 
 var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
 
-var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(source, true).forEach(function (key) { (0, _defineProperty2["default"])(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(source).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
 var prettier = require('prettier/standalone');
 
@@ -30,74 +32,64 @@ var _require2 = require('./common'),
     addWhiteListModule = _require2.addWhiteListModule,
     removeWhiteListModule = _require2.removeWhiteListModule;
 
-var transpile =
-/*#__PURE__*/
-function () {
-  var _ref = (0, _asyncToGenerator2["default"])(
-  /*#__PURE__*/
-  _regenerator["default"].mark(function _callee(src) {
-    var options,
-        _options$minify,
-        minify,
-        _options$minifyOpts,
-        minifyOpts,
-        _options$prettier,
-        prettier,
-        _options$prettierOpts,
-        prettierOpts,
-        _options$context,
-        context,
-        _options$buildOptions,
-        buildOptions,
-        project,
-        _args = arguments;
+var transpile = function transpile(src) {
+  var options,
+      _options$minify,
+      minify,
+      _options$minifyOpts,
+      minifyOpts,
+      _options$prettier,
+      prettier,
+      _options$prettierOpts,
+      prettierOpts,
+      _options$context,
+      context,
+      _options$buildOptions,
+      buildOptions,
+      project,
+      _args = arguments;
 
-    return _regenerator["default"].wrap(function _callee$(_context) {
-      while (1) {
-        switch (_context.prev = _context.next) {
-          case 0:
-            options = _args.length > 1 && _args[1] !== undefined ? _args[1] : {};
-            _options$minify = options.minify, minify = _options$minify === void 0 ? false : _options$minify, _options$minifyOpts = options.minifyOpts, minifyOpts = _options$minifyOpts === void 0 ? {} : _options$minifyOpts, _options$prettier = options.prettier, prettier = _options$prettier === void 0 ? false : _options$prettier, _options$prettierOpts = options.prettierOpts, prettierOpts = _options$prettierOpts === void 0 ? {} : _options$prettierOpts, _options$context = options.context, context = _options$context === void 0 ? '/' : _options$context, _options$buildOptions = options.buildOptions, buildOptions = _options$buildOptions === void 0 ? {} : _options$buildOptions, project = options.project; // The decorated plugins should append this, but for now we add here to simplify
-            // src += ';const __contract = new __contract_name();const __metadata = {}'
-            // then, babelify it
+  return _regenerator["default"].async(function transpile$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          options = _args.length > 1 && _args[1] !== undefined ? _args[1] : {};
+          _options$minify = options.minify, minify = _options$minify === void 0 ? false : _options$minify, _options$minifyOpts = options.minifyOpts, minifyOpts = _options$minifyOpts === void 0 ? {} : _options$minifyOpts, _options$prettier = options.prettier, prettier = _options$prettier === void 0 ? false : _options$prettier, _options$prettierOpts = options.prettierOpts, prettierOpts = _options$prettierOpts === void 0 ? {} : _options$prettierOpts, _options$context = options.context, context = _options$context === void 0 ? '/' : _options$context, _options$buildOptions = options.buildOptions, buildOptions = _options$buildOptions === void 0 ? {} : _options$buildOptions, project = options.project; // The decorated plugins should append this, but for now we add here to simplify
+          // src += ';const __contract = new __contract_name();const __metadata = {}'
+          // then, babelify it
 
-            src = babelify(src, [plugin]); // remove flow types
+          src = babelify(src, [plugin]); // remove flow types
 
-            src = babelify(src, [flowPlugin]); // don't know, maybe babel not support decorators along to private property
+          src = babelify(src, [flowPlugin]); // don't know, maybe babel not support decorators along to private property
 
-            _context.next = 6;
-            return transform(src, context, project, buildOptions);
+          _context.next = 6;
+          return _regenerator["default"].awrap(transform(src, context, project, buildOptions));
 
-          case 6:
-            src = _context.sent;
-            // finally, wrap it
-            src = makeWrapper(src).trim(); // preparation for minified
+        case 6:
+          src = _context.sent;
+          // finally, wrap it
+          src = makeWrapper(src).trim(); // preparation for minified
 
-            src = prettify(src, {
-              semi: true
-            });
+          src = prettify(src, {
+            semi: true
+          });
 
-            if (prettier) {
-              src = prettify(src, prettierOpts);
-            } else if (minify) {
-              src = doMinify(src, minifyOpts);
-            } // console.log(src)
+          if (prettier) {
+            src = prettify(src, prettierOpts);
+          } else if (minify) {
+            src = doMinify(src, minifyOpts);
+          } // console.log(src)
 
 
-            return _context.abrupt("return", src);
+          return _context.abrupt("return", src);
 
-          case 11:
-          case "end":
-            return _context.stop();
-        }
+        case 11:
+        case "end":
+          return _context.stop();
       }
-    }, _callee);
-  }));
-
-  return function transpile(_x) {
-    return _ref.apply(this, arguments);
-  };
-}();
+    }
+  });
+};
 
 var simpleTranspile = function simpleTranspile(src) {
   var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
@@ -141,7 +133,7 @@ function prettify(src) {
 
 function doMinify(src) {
   var opts = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
-  var result = Terser.minify(src, (0, _objectSpread2["default"])({
+  var result = Terser.minify(src, _objectSpread({
     parse: {
       bare_returns: true
     },
--- dist/transform/index.js
+++ dist/transform/index.js
@@ -4,8 +4,6 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 
 var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
 
-var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));
-
 var url = require('url');
 
 var fs = require('fs');
@@ -38,264 +36,244 @@ var babelify = require('./babelify');
  */
 
 
-exports.transform =
-/*#__PURE__*/
-function () {
-  var _ref = (0, _asyncToGenerator2["default"])(
-  /*#__PURE__*/
-  _regenerator["default"].mark(function _callee2(src) {
-    var context,
-        project,
-        options,
-        parsed,
-        requires,
-        _args2 = arguments;
-    return _regenerator["default"].wrap(function _callee2$(_context2) {
-      while (1) {
-        switch (_context2.prev = _context2.next) {
-          case 0:
-            context = _args2.length > 1 && _args2[1] !== undefined ? _args2[1] : '/';
-            project = _args2.length > 2 && _args2[2] !== undefined ? _args2[2] : null;
-            options = _args2.length > 3 && _args2[3] !== undefined ? _args2[3] : {};
-            _context2.next = 5;
-            return babelify(src, [importToRequire]);
+exports.transform = function _callee2(src) {
+  var context,
+      project,
+      options,
+      parsed,
+      requires,
+      _args2 = arguments;
+  return _regenerator["default"].async(function _callee2$(_context2) {
+    while (1) {
+      switch (_context2.prev = _context2.next) {
+        case 0:
+          context = _args2.length > 1 && _args2[1] !== undefined ? _args2[1] : '/';
+          project = _args2.length > 2 && _args2[2] !== undefined ? _args2[2] : null;
+          options = _args2.length > 3 && _args2[3] !== undefined ? _args2[3] : {};
+          _context2.next = 5;
+          return _regenerator["default"].awrap(babelify(src, [importToRequire]));
 
-          case 5:
-            src = _context2.sent;
-            parsed = babelParser.parse(src, {
-              sourceType: 'module',
-              plugins: plugins
-            });
-            requires = {};
-            traverse(parsed, {
-              CallExpression: function CallExpression(_ref2) {
-                var node = _ref2.node;
+        case 5:
+          src = _context2.sent;
+          parsed = babelParser.parse(src, {
+            sourceType: 'module',
+            plugins: plugins
+          });
+          requires = {};
+          traverse(parsed, {
+            CallExpression: function CallExpression(_ref) {
+              var node = _ref.node;
 
-                if (!node || node.callee.name !== 'require') {
-                  return;
-                }
-
-                var arguments_ = node.arguments;
-
-                if (arguments_.length !== 1 || arguments_[0].type !== 'StringLiteral') {
-                  return;
-                }
-
-                var value = arguments_[0].value;
-                requires[value] = value;
+              if (!node || node.callee.name !== 'require') {
+                return;
               }
-            });
-            _context2.next = 11;
-            return Promise.all(Object.keys(requires).map(
-            /*#__PURE__*/
-            function () {
-              var _ref3 = (0, _asyncToGenerator2["default"])(
-              /*#__PURE__*/
-              _regenerator["default"].mark(function _callee(value) {
-                var _data, _data2, localFlag, moduleName, filePath, data;
 
-                return _regenerator["default"].wrap(function _callee$(_context) {
-                  while (1) {
-                    switch (_context.prev = _context.next) {
-                      case 0:
-                        if (!(isWhitelistModule(value) || options.remote && options.remote[value])) {
-                          _context.next = 3;
-                          break;
-                        }
+              var arguments_ = node.arguments;
 
-                        delete requires[value];
-                        return _context.abrupt("return");
+              if (arguments_.length !== 1 || arguments_[0].type !== 'StringLiteral') {
+                return;
+              }
 
-                      case 3:
-                        if (!isHttp(value)) {
-                          _context.next = 17;
-                          break;
-                        }
+              var value = arguments_[0].value;
+              requires[value] = value;
+            }
+          });
+          _context2.next = 11;
+          return _regenerator["default"].awrap(Promise.all(Object.keys(requires).map(function _callee(value) {
+            var _data, _data2, localFlag, moduleName, filePath, data;
 
-                        if (['.js', '.json'].includes(path.extname(value))) {
-                          _context.next = 6;
-                          break;
-                        }
-
-                        throw new Error('"require" supports only .js and .json files.');
-
-                      case 6:
-                        _context.next = 8;
-                        return axios.get(value);
-
-                      case 8:
-                        _data = _context.sent.data;
-
-                        if (!(typeof _data === 'string')) {
-                          _context.next = 15;
-                          break;
-                        }
-
-                        _context.next = 12;
-                        return exports.transform(_data, value, null, options);
-
-                      case 12:
-                        requires[value] = _context.sent;
-                        _context.next = 16;
-                        break;
-
-                      case 15:
-                        requires[value] = _data;
-
-                      case 16:
-                        return _context.abrupt("return");
-
-                      case 17:
-                        if (!isHttp(context)) {
-                          _context.next = 33;
-                          break;
-                        }
-
-                        if (!isNodeModule(value)) {
-                          _context.next = 20;
-                          break;
-                        }
-
-                        throw new Error('Cannot use node_modules in remote URL.');
-
-                      case 20:
-                        if (['.js', '.json'].includes(path.extname(value))) {
-                          _context.next = 22;
-                          break;
-                        }
-
-                        throw new Error('"require" supports only .js and .json files.');
-
-                      case 22:
-                        _context.next = 24;
-                        return axios.get(url.resolve(context, value));
-
-                      case 24:
-                        _data2 = _context.sent.data;
-
-                        if (!(typeof _data2 === 'string')) {
-                          _context.next = 31;
-                          break;
-                        }
-
-                        _context.next = 28;
-                        return exports.transform(_data2, url.resolve(context, value), null, options);
-
-                      case 28:
-                        requires[value] = _context.sent;
-                        _context.next = 32;
-                        break;
-
-                      case 31:
-                        requires[value] = _data2;
-
-                      case 32:
-                        return _context.abrupt("return");
-
-                      case 33:
-                        // if you want to use bundle instead of blockchain node_modules
-                        localFlag = '@local';
-                        moduleName = value;
-
-                        if (moduleName.endsWith(localFlag)) {
-                          moduleName = moduleName.slice(0, -localFlag.length);
-                        }
-
-                        if (isNodeModule(moduleName)) {
-                          filePath = require.resolve("".concat(moduleName)); // to ignore webpack warning
-                        } else {
-                          if (project) {
-                            filePath = path.join(context, value);
-                          } else {
-                            filePath = require.resolve("".concat(path.resolve(context, value)));
-                          }
-                        }
-
-                        if (['.js', '.json'].includes(path.extname(filePath))) {
-                          _context.next = 39;
-                          break;
-                        }
-
-                        throw new Error('"require" supports only .js and .json files.');
-
-                      case 39:
-                        if (project) {
-                          data = project.getFile(filePath).getData().toString();
-                        } else {
-                          data = fs.readFileSync(filePath).toString();
-                        }
-
-                        _context.prev = 40;
-                        data = JSON.parse(data);
-                        requires[value] = data;
-                        _context.next = 54;
-                        break;
-
-                      case 45:
-                        _context.prev = 45;
-                        _context.t0 = _context["catch"](40);
-
-                        if (!(_context.t0 instanceof SyntaxError)) {
-                          _context.next = 53;
-                          break;
-                        }
-
-                        _context.next = 50;
-                        return exports.transform(data, path.dirname(filePath), project, options);
-
-                      case 50:
-                        requires[value] = _context.sent;
-                        _context.next = 54;
-                        break;
-
-                      case 53:
-                        throw _context.t0;
-
-                      case 54:
-                      case "end":
-                        return _context.stop();
+            return _regenerator["default"].async(function _callee$(_context) {
+              while (1) {
+                switch (_context.prev = _context.next) {
+                  case 0:
+                    if (!(isWhitelistModule(value) || options.remote && options.remote[value])) {
+                      _context.next = 3;
+                      break;
                     }
-                  }
-                }, _callee, null, [[40, 45]]);
-              }));
 
-              return function (_x2) {
-                return _ref3.apply(this, arguments);
-              };
-            }()));
+                    delete requires[value];
+                    return _context.abrupt("return");
 
-          case 11:
-            if (!(Object.keys(requires).length === 0)) {
-              _context2.next = 13;
-              break;
-            }
+                  case 3:
+                    if (!isHttp(value)) {
+                      _context.next = 17;
+                      break;
+                    }
 
-            return _context2.abrupt("return", src);
+                    if (['.js', '.json'].includes(path.extname(value))) {
+                      _context.next = 6;
+                      break;
+                    }
 
-          case 13:
-            _context2.next = 15;
-            return babelify(src, [resolveExternal(requires)]);
+                    throw new Error('"require" supports only .js and .json files.');
 
-          case 15:
-            src = _context2.sent;
+                  case 6:
+                    _context.next = 8;
+                    return _regenerator["default"].awrap(axios.get(value));
 
-            if (src.endsWith(';')) {
-              src = src.slice(0, -1); // for redundancy Semicolon
-            }
+                  case 8:
+                    _data = _context.sent.data;
 
-            return _context2.abrupt("return", src);
+                    if (!(typeof _data === 'string')) {
+                      _context.next = 15;
+                      break;
+                    }
 
-          case 18:
-          case "end":
-            return _context2.stop();
-        }
+                    _context.next = 12;
+                    return _regenerator["default"].awrap(exports.transform(_data, value, null, options));
+
+                  case 12:
+                    requires[value] = _context.sent;
+                    _context.next = 16;
+                    break;
+
+                  case 15:
+                    requires[value] = _data;
+
+                  case 16:
+                    return _context.abrupt("return");
+
+                  case 17:
+                    if (!isHttp(context)) {
+                      _context.next = 33;
+                      break;
+                    }
+
+                    if (!isNodeModule(value)) {
+                      _context.next = 20;
+                      break;
+                    }
+
+                    throw new Error('Cannot use node_modules in remote URL.');
+
+                  case 20:
+                    if (['.js', '.json'].includes(path.extname(value))) {
+                      _context.next = 22;
+                      break;
+                    }
+
+                    throw new Error('"require" supports only .js and .json files.');
+
+                  case 22:
+                    _context.next = 24;
+                    return _regenerator["default"].awrap(axios.get(url.resolve(context, value)));
+
+                  case 24:
+                    _data2 = _context.sent.data;
+
+                    if (!(typeof _data2 === 'string')) {
+                      _context.next = 31;
+                      break;
+                    }
+
+                    _context.next = 28;
+                    return _regenerator["default"].awrap(exports.transform(_data2, url.resolve(context, value), null, options));
+
+                  case 28:
+                    requires[value] = _context.sent;
+                    _context.next = 32;
+                    break;
+
+                  case 31:
+                    requires[value] = _data2;
+
+                  case 32:
+                    return _context.abrupt("return");
+
+                  case 33:
+                    // if you want to use bundle instead of blockchain node_modules
+                    localFlag = '@local';
+                    moduleName = value;
+
+                    if (moduleName.endsWith(localFlag)) {
+                      moduleName = moduleName.slice(0, -localFlag.length);
+                    }
+
+                    if (isNodeModule(moduleName)) {
+                      filePath = require.resolve("".concat(moduleName)); // to ignore webpack warning
+                    } else {
+                      if (project) {
+                        filePath = path.join(context, value);
+                      } else {
+                        filePath = require.resolve("".concat(path.resolve(context, value)));
+                      }
+                    }
+
+                    if (['.js', '.json'].includes(path.extname(filePath))) {
+                      _context.next = 39;
+                      break;
+                    }
+
+                    throw new Error('"require" supports only .js and .json files.');
+
+                  case 39:
+                    if (project) {
+                      data = project.getFile(filePath).getData().toString();
+                    } else {
+                      data = fs.readFileSync(filePath).toString();
+                    }
+
+                    _context.prev = 40;
+                    data = JSON.parse(data);
+                    requires[value] = data;
+                    _context.next = 54;
+                    break;
+
+                  case 45:
+                    _context.prev = 45;
+                    _context.t0 = _context["catch"](40);
+
+                    if (!(_context.t0 instanceof SyntaxError)) {
+                      _context.next = 53;
+                      break;
+                    }
+
+                    _context.next = 50;
+                    return _regenerator["default"].awrap(exports.transform(data, path.dirname(filePath), project, options));
+
+                  case 50:
+                    requires[value] = _context.sent;
+                    _context.next = 54;
+                    break;
+
+                  case 53:
+                    throw _context.t0;
+
+                  case 54:
+                  case "end":
+                    return _context.stop();
+                }
+              }
+            }, null, null, [[40, 45]]);
+          })));
+
+        case 11:
+          if (!(Object.keys(requires).length === 0)) {
+            _context2.next = 13;
+            break;
+          }
+
+          return _context2.abrupt("return", src);
+
+        case 13:
+          _context2.next = 15;
+          return _regenerator["default"].awrap(babelify(src, [resolveExternal(requires)]));
+
+        case 15:
+          src = _context2.sent;
+
+          if (src.endsWith(';')) {
+            src = src.slice(0, -1); // for redundancy Semicolon
+          }
+
+          return _context2.abrupt("return", src);
+
+        case 18:
+        case "end":
+          return _context2.stop();
       }
-    }, _callee2);
-  }));
-
-  return function (_x) {
-    return _ref.apply(this, arguments);
-  };
-}();
+    }
+  });
+};
 
 exports.babelify = babelify;
